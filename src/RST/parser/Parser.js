87c51ac85e79013cc60f1a1bc54fd6057c112138
Andrew Ligon <andrew.ligon@nextthought.com>
2016-12-09 12:00:25 -0600

    Clean up, and add some documentation

--- src/RST/parser/Parser.js
+++ src/RST/parser/Parser.js
@@ -2,32 +2,71 @@ const BLOCK_TYPES = Symbol('Block Types');
 
 export default class Parser {
 	constructor () {
+		//TODO: decide if we need to formalize the context beyond a simple object
 		this.context = {};
-		this.parsedBlocks = [];
-		this.currentBlock = null;
 
-		this[BLOCK_TYPES] = this.getBlockClasses();
+		this[BLOCK_TYPES] = this.getBlockTypes();
 	}
 
-
-	getBlockClasses () {
+	/**
+	 * Return the list of types to check the parts of the input against, the types
+	 * will be checked in order.
+	 * At a minimum the types need to have:
+	 *
+	 * isTypeForBlock(block, context) method to see if this is the block for a part or not
+	 *
+	 * parse(block, context, currentBlock) method to parse the block
+	 *     returns an object {block: the parsed block, context: the new context for the parser}
+	 *     both properties are optional
+	 *
+	 * They can define optionally:
+	 *
+	 * shouldAppendBlock(block, context) method to see if the next block should be appended or not
+	 *
+	 * appendBlock(block, context) method to append the block to this block.
+	 *     should have the same return value as parse
+	 *
+	 * @return {[Object]} the list of block types
+	 */
+	getBlockTypes () {
 		return [];
 	}
 
-
-	parseBlocksFromInput (input) {
+	/**
+	 * Given the input into the parser, format it into an iterable object
+	 * of the blocks in the input to parse.
+	 *
+	 * @param  {Mixed} input what to parse
+	 * @return {Array}       the blocks in the input
+	 */
+	formatInput (input) {
 		return input;
 	}
 
-
-	joinBlocks (blocks/*, context*/) {
-		return blocks;
+	/**
+	 * After running through the parse, give subclasses a chance to control
+	 * the format of the output.
+	 *
+	 * @param {Object} parsed the result of parsing
+	 * @param {Array} parsedBlocks the blocks that were parsed from the input
+	 * @param {Object} context the context set while parsing the blocks
+	 * @return {Object}        the formatted version to output
+	 */
+	formatParsed (parsed) {
+		return parsed;
 	}
 
 
+	/**
+	 * Run an input through the parser.
+	 *
+	 * Return the parsedBlocks and the context of the parser
+	 *
+	 * @param  {Mixed} input what to parse
+	 * @return {Object}       the result of parsing
+	 */
 	parse (input) {
-		debugger;
-		const blocks = this.parseBlocksFromInput(input);
+		const blocks = this.formatInput(input);
 
 		let context = {};
 		let parsedBlocks = [];
@@ -44,16 +83,23 @@ export default class Parser {
 			}
 		}
 
-		return {
-			parsed: this.joinBlocks(parsedBlocks, context),
-			...context
-		};
+		return this.formatParsed({
+			parsedBlocks,
+			context
+		});
 	}
 
-
-	getClassForBlock (block, context) {
+	/**
+	 * Given a block from the input determine which type to use
+	 * to parse it.
+	 *
+	 * @param  {Mixed} blockInput   a block from the input
+	 * @param  {Object} context the context of the parser
+	 * @return {Object}        the block to use to parse
+	 */
+	getClassForBlock (blockInput, context) {
 		for (let blockType of this[BLOCK_TYPES]) {
-			if (blockType.isTypeForBlock(block, context)) {
+			if (blockType.isTypeForBlock(blockInput, context)) {
 				return blockType;
 			}
 		}
