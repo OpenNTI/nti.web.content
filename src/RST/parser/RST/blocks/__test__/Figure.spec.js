13ec459f791a5368557079e2659dd482df8fdd71
Andrew Ligon <andrew.ligon@nextthought.com>
2016-12-29 10:50:10 -0600

    Add a placeholder test for figures

---
+++ src/RST/parser/RST/blocks/__test__/Figure.spec.js
@@ -0,0 +1,7 @@
+describe('Figure', () => {
+	it('Placeholder', () => {
+		//TODO: write this test once we figure out how we should parse the caption
+		//and body
+		expect(false).toBeTruthy();
+	});
+});
