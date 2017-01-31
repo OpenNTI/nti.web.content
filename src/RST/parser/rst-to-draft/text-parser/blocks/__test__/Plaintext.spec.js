e5bf7eca8f5f2c9723481a7f0ce71ff5025821d5
Andrew Ligon <andrew.ligon@nextthought.com>
2017-01-31 14:28:50 -0600

    Test the output of plaintext

--- src/RST/parser/rst-to-draft/text-parser/blocks/__test__/Plaintext.spec.js
+++ src/RST/parser/rst-to-draft/text-parser/blocks/__test__/Plaintext.spec.js
@@ -83,4 +83,68 @@ fdescribe('Plaintext', () => {
 			expect(block.shouldAppendBlock(newBlock)).toBeFalsy();
 		});
 	});
+
+	describe('appendBlock', () => {
+		it('appends to the existing text', () => {
+			const block = new Plaintext('n');
+			const newBlock = new Plaintext('o');
+
+			block.appendBlock(newBlock);
+
+			expect(block.text).toEqual('no');
+		});
+	});
+
+
+	describe('getOutput', () => {
+		function buildMarker () {
+			const block = {
+				getOutputForInterpreted: () => {}
+			};
+
+			spyOn(block, 'getOutputForInterpreted');
+
+			return block;
+		}
+
+		it('If it has role marker and not forced, calls marker', () => {
+			const block = new Plaintext('n');
+			const marker = buildMarker();
+			const context = {};
+
+			block.setRoleMarker(marker);
+			block.getOutput(context);
+
+			expect(marker.getOutputForInterpreted).toHaveBeenCalledWith(block, context);
+		});
+
+		it('If it has role marker and forces, does not call marker', () => {
+			const block = new Plaintext('n');
+			const marker = buildMarker();
+			const context = {};
+
+			block.setRoleMarker(marker);
+			block.getOutput(context, true);
+
+			expect(marker.getOutputForInterpreted).not.toHaveBeenCalled();
+		});
+
+		it('No role marker, has the correct output', () => {
+			const block = new Plaintext('p');
+
+			block.appendBlock(new Plaintext('l'));
+			block.appendBlock(new Plaintext('a'));
+			block.appendBlock(new Plaintext('i'));
+			block.appendBlock(new Plaintext('n'));
+			block.appendBlock(new Plaintext('t'));
+			block.appendBlock(new Plaintext('e'));
+			block.appendBlock(new Plaintext('x'));
+			block.appendBlock(new Plaintext('t'));
+
+			const {output, context} = block.getOutput({charCount: 0});
+
+			expect(output).toEqual('plaintext');
+			expect(context.charCount).toEqual(9);
+		});
+	});
 });
