3335dcc38b63e679305a53c2d9045c549f3aacd2
Andrew Ligon <andrew.ligon@nextthought.com>
2017-02-15 19:27:00 -0600

    add a place holder for the meta editor

--- src/editor/meta-editor/View.jsx
+++ src/editor/meta-editor/View.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MetaEditor ({content, course}) {
 	return (
-		<div>
+		<div className="content-meta-editor">
 			<span>Meta Editor</span>
 		</div>
 	);
