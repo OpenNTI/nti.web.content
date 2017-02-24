5b1ceddb92bdf182747a3a5cbf27080749c55da3
Andrew Ligon <andrew.ligon@nextthought.com>
2017-02-23 18:40:40 -0600

    Show messages while the content is publishing

--- src/editor/editor-panel/RenderingMask.jsx
+++ src/editor/editor-panel/RenderingMask.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import cx from 'classnames';
+import {scoped} from 'nti-lib-locale';
+import {Sequence, Loading} from 'nti-web-commons';
 
 import {PUBLISHING} from '../Constants';
 import Store from '../Store';
+
+const DEFAULT_TEXT = {
+	publishingMessageOne: 'Publishing...',
+	publishingMessageTwo: 'This may take a moment...',
+	publishingMessageThree: {
+		message: 'We are experiencing a high volume of submissions. Your reading was added to a queue. Please come back later.',
+		button: 'Okay'
+	}
+};
+
+const t = scoped('CONTENT_EDITOR_RENDERING_MASK', DEFAULT_TEXT);
 
 export default class ContentEditorRenderingMask extends React.Component {
 	static propTypes = {
 		contentPackage: React.PropTypes.object
 	}
 
-	state = {}
+	state = {
+		publising: true
+	}
 
 	componentDidMount () {
 		Store.addChangeListener(this.onStoreChange);
@@ -49,12 +64,16 @@ export default class ContentEditorRenderingMask extends React.Component {
 
 	onRenderJobChange = (job) => {
 		const {publishing} = this.state;
+		let state = {
+			failed: job.isFailed,
+			success: job.isSucess
+		};
 
 		if (publishing !== job.isPending) {
-			this.setState({
-				publishing: job.isPending
-			});
+			state.publishing = job.isPending;
 		}
+
+		this.setState(state);
 	}
 
 
@@ -85,14 +104,37 @@ export default class ContentEditorRenderingMask extends React.Component {
 
 		return (
 			<div className={cls}>
-				{publishing ?
+				{!publishing ?
 						null :
-						(
-							<div>
-								<span>Rendering Mask</span>
-							</div>
-						)
+						this.renderPublishing()
 				}
+			</div>
+		);
+	}
+
+	renderPublishing = () => {
+		return (
+			<div className="publishing-inidcator">
+				<Sequence.Timed>
+					<Sequence.Item showFor={9000}>
+						<div className="spinner-message">
+							<Loading.Spinner className="spinner" size="150px" strokeWidth="1" />
+							<span className="spinner-message">{t('publishingMessageOne')}</span>
+						</div>
+					</Sequence.Item>
+					<Sequence.Item showFor={12000}>
+						<div className="spinner-message">
+							<Loading.Spinner className="spinner" size="150px" strokeWidth="1" />
+							<span className="spinner-message">{t('publishingMessageTwo')}</span>
+						</div>
+					</Sequence.Item>
+					<Sequence.Item showFor={Infinity}>
+						<div className="too-long-message">
+							<span className="message">{t('publishingMessageThree.message')}</span>
+							<span className="button">{t('publishingMessageThree.button')}</span>
+						</div>
+					</Sequence.Item>
+				</Sequence.Timed>
 			</div>
 		);
 	}
