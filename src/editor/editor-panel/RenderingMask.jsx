182fac9c7766c767a5a733983dc43eb90afd1671
Andrew Ligon <andrew.ligon@nextthought.com>
2017-02-23 21:59:52 -0600

    Add failure and sucess states to the render

--- src/editor/editor-panel/RenderingMask.jsx
+++ src/editor/editor-panel/RenderingMask.jsx
@@ -12,14 +12,18 @@ const DEFAULT_TEXT = {
 	publishingMessageThree: {
 		message: 'We are experiencing a high volume of submissions. Your reading was added to a queue. Please come back later.',
 		button: 'Okay'
-	}
+	},
+	successMessage: 'Successfully Published!',
+	failureMessage: 'Failed To Publish.'
 };
 
 const t = scoped('CONTENT_EDITOR_RENDERING_MASK', DEFAULT_TEXT);
 
 export default class ContentEditorRenderingMask extends React.Component {
 	static propTypes = {
-		contentPackage: React.PropTypes.object
+		contentPackage: React.PropTypes.object,
+		onSuccessfulPublish: React.PropTypes.func,
+		onFailureFinish: React.PropTypes.func
 	}
 
 	state = {
@@ -98,40 +102,110 @@ export default class ContentEditorRenderingMask extends React.Component {
 	}
 
 
+	onSuccessFinish = () => {
+		const {onSuccessfulPublish} = this.props;
+
+		if (onSuccessfulPublish) {
+			onSuccessfulPublish();
+		} else {
+			this.setState({
+				publishing: false,
+				success: false,
+				failure: false
+			});
+		}
+	}
+
+
+	onFailureFinish = () => {
+		const {onFailureFinish} = this.props;
+
+		if (onFailureFinish) {
+			onFailureFinish();
+		} else {
+			this.setState({
+				publishing: false,
+				success: false,
+				failure: false
+			});
+		}
+	}
+
+
+	onPublishDismiss = () => {
+		this.setState({
+			publishing: false,
+			success: false,
+			failure: false
+		});
+	}
+
+
 	render () {
-		const {publishing} = this.state;
-		const cls = cx('content-editor-render-mask', {publishing});
+		const {publishing, success, failure} = this.state;
+		const cls = cx('content-editor-render-mask', {publishing, success, failure});
 
 		return (
 			<div className={cls}>
-				{!publishing ?
-						null :
-						this.renderPublishing()
-				}
+				{publishing ? this.renderPublishing() : null}
+				{success ? this.renderSuccess() : null}
+				{failure ? this.renderFailure() : null}
 			</div>
 		);
 	}
 
 	renderPublishing = () => {
 		return (
-			<div className="publishing-inidcator">
+			<div className="publishing-indicator">
 				<Sequence.Timed>
 					<Sequence.Item showFor={9000}>
 						<div className="spinner-message">
-							<Loading.Spinner className="spinner" size="150px" strokeWidth="1" />
+							<Loading.Spinner className="spinner" size="120px" strokeWidth="1" />
 							<span className="spinner-message">{t('publishingMessageOne')}</span>
 						</div>
 					</Sequence.Item>
 					<Sequence.Item showFor={12000}>
 						<div className="spinner-message">
-							<Loading.Spinner className="spinner" size="150px" strokeWidth="1" />
+							<Loading.Spinner className="spinner" size="120px" strokeWidth="1" />
 							<span className="spinner-message">{t('publishingMessageTwo')}</span>
 						</div>
 					</Sequence.Item>
 					<Sequence.Item showFor={Infinity}>
 						<div className="too-long-message">
 							<span className="message">{t('publishingMessageThree.message')}</span>
-							<span className="button">{t('publishingMessageThree.button')}</span>
+							<span className="button" onClick={this.onPublishDismiss}>{t('publishingMessageThree.button')}</span>
+						</div>
+					</Sequence.Item>
+				</Sequence.Timed>
+			</div>
+		);
+	}
+
+
+	renderSuccess = () => {
+		return (
+			<div className="success-indicator">
+				<Sequence.Timed onFinish={this.onSuccessFinish}>
+					<Sequence.Item showFor={5000}>
+						<div className="success">
+							<i className="icon-check" />
+							<span>{t('successMessage')}</span>
+						</div>
+					</Sequence.Item>
+				</Sequence.Timed>
+			</div>
+		);
+	}
+
+
+	renderFailure = () => {
+		return (
+			<div className="failure-indicator">
+				<Sequence.Timed onFinish={this.onFailureFinish}>
+					<Sequence.Item showFor={5000}>
+						<div className="failure">
+							<span className="icon">!</span>
+							<span>{t('failureMessage')}</span>
 						</div>
 					</Sequence.Item>
 				</Sequence.Timed>
