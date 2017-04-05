b8a8284e63de4cacc7ea2ce0fee17ab07654d460
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-04 20:55:31 -0500

    Return the last cmp in the editor to align to

--- src/draft-core/plugins/external-links/utils/get-cmp-for-state.js
+++ src/draft-core/plugins/external-links/utils/get-cmp-for-state.js
@@ -1,6 +1,39 @@
-export default function getCmpForState (cmps/*, state*/) {
-	//TODO: look into when there is more than one component
-	//using the state to pick the correct one.
+import DraftOffsetKey from 'draft-js/lib/DraftOffsetKey';
 
-	return cmps[0];
+function getBlockKeyToIndex (editorState) {
+	const content = editorState.getCurrentContent();
+
+	return content
+			.getBlocksAsArray()
+			.reduce((acc, block, index) => {
+				acc[block.key] = index;
+
+				return acc;
+			}, {});
+}
+
+
+function getInfoForCmp (cmp) {
+	return DraftOffsetKey.decode(cmp.offsetKey);
+}
+
+export default function getCmpForState (cmps, editorState) {
+	const blockMap = getBlockKeyToIndex(editorState);
+	const sorted = cmps.sort((a, b) => {
+		a = getInfoForCmp(a);
+		b = getInfoForCmp(b);
+
+		const aBlockIndex = blockMap[a.blockKey];
+		const bBlockIndex = blockMap[b.blockKey];
+
+		const aDecorator = a.decoratorKey;
+		const bDecorator = b.decoratorKey;
+
+		const blockSort = aBlockIndex < bBlockIndex ? -1 : aBlockIndex === bBlockIndex ? 0 : 1;
+		const decoratorSort = aDecorator < bDecorator ? -1 : aDecorator === bDecorator ? 0 : 1;
+
+		return blockSort === 0 ? decoratorSort : blockSort;
+	});
+
+	return sorted[sorted.length - 1];
 }
