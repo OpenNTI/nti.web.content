d3720a69c515179daa41bc3650fb1597eef24b02
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-06 22:53:34 -0500

    Update the selection to cover the change in text

--- src/draft-core/plugins/external-links/utils/replace-entity-text-at-offset.js
+++ src/draft-core/plugins/external-links/utils/replace-entity-text-at-offset.js
@@ -1,16 +1,31 @@
-import {Modifier, EditorState} from 'draft-js';
+import {Modifier, EditorState, SelectionState} from 'draft-js';
 
 import getSelectionForEntityKeyAtOffset from './get-selection-for-entity-key-at-offset';
+
+
+function getNewSelection (text, selection) {
+	const anchorOffset = selection.getAnchorOffset();
+
+	return new SelectionState({
+		anchorKey: selection.getAnchorKey(),
+		anchorOffset,
+		focusKey: selection.getFocusKey(),
+		focusOffset: anchorOffset + text.length,
+		isBackward: false,
+		hasFocus: selection.getHasFocus()
+	});
+}
 
 export default function replaceEntityTextAtOffset (text, entityKey, offset, editorState) {
 	const selection = getSelectionForEntityKeyAtOffset(entityKey, offset, editorState);
 	const content = editorState.getCurrentContent();
 	const newContent = Modifier.replaceText(content, selection, text, void 0, entityKey);
+	const newSelection = getNewSelection(text, selection);
 
 	return EditorState.create({
 		currentContent: newContent,
 		allowUndo: editorState.getAllowUndo(),
 		decorator: editorState.getDecorator(),
-		selection
+		selection: newSelection
 	});
 }
