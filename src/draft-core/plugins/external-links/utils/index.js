ea46730ce0abbf027ef2ee01dc895d51ec0baad1
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-07 17:31:30 -0500

    If creating an new link with a link in the selection copy the href

--- src/draft-core/plugins/external-links/utils/index.js
+++ src/draft-core/plugins/external-links/utils/index.js
@@ -1,6 +1,7 @@
 export createEntity from './create-entity';
 export createNewLinkAtOffset from './create-new-link-at-offset';
 export getCmpForState from './get-cmp-for-state';
+export getFirstSelectedEntityHref from './get-first-selected-entity-href';
 export getFullHref from './get-full-href';
 export getSelectedEntityKey from './get-selected-entity-key';
 export getSelectionForEntity from './get-selection-for-entity-key-at-offset';
