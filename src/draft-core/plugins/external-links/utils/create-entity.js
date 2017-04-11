6fa6ceed5e2dcd5e86d09d22adb6e4e0c586002d
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-11 16:09:11 -0500

    Typo

--- src/draft-core/plugins/external-links/utils/create-entity.js
+++ src/draft-core/plugins/external-links/utils/create-entity.js
@@ -5,5 +5,5 @@ import {ENTITIES, MUTABILITY} from '../../../Constants';
 export default function createLink (href) {
 	//If the contiguous-entity plugin is included the contigous false flag will prevent
 	//the entity from expanding if you type after it
-	return Entity.create(ENTITIES.LINK, MUTABILITY.MUTABLE, {href, contigous: false});
+	return Entity.create(ENTITIES.LINK, MUTABILITY.MUTABLE, {href, contiguous: false});
 }
