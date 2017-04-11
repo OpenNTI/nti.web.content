fa2dfbf5771f6a93bbb099ebe192e05a053e8c81
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-11 17:56:48 -0500

    NTI-3193 Only allow links in certain block types

--- src/draft-core/plugins/external-links/utils/index.js
+++ src/draft-core/plugins/external-links/utils/index.js
@@ -6,6 +6,7 @@ export getFullHref from './get-full-href';
 export getSelectedEntityKey from './get-selected-entity-key';
 export getSelectionForEntity from './get-selection-for-entity-key-at-offset';
 export isEntityAtOffsetInSingleBlock from './is-entity-at-offset-in-single-block';
+export isValidSelectionForLink from './is-valid-selection-for-link';
 export removeEntityKeyAtOffset from './remove-entity-key-at-offset';
 export replaceEntityTextAtOffset from './replace-entity-text-at-offset';
 export replaceEntityTextAtSelection from './replace-entity-text-at-selection';
