a96546c92add7b43436971f6cc441c69a977c041
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-11 15:59:43 -0500

    NTI-3192 Mark links as "non-contiguous"

--- src/draft-core/plugins/external-links/utils/create-entity.js
+++ src/draft-core/plugins/external-links/utils/create-entity.js
@@ -3,5 +3,7 @@ import {Entity} from 'draft-js';
 import {ENTITIES, MUTABILITY} from '../../../Constants';
 
 export default function createLink (href) {
-	return Entity.create(ENTITIES.LINK, MUTABILITY.MUTABLE, {href});
+	//If the contiguous-entity plugin is included the contigous false flag will prevent
+	//the entity from expanding if you type after it
+	return Entity.create(ENTITIES.LINK, MUTABILITY.MUTABLE, {href, contigous: false});
 }
