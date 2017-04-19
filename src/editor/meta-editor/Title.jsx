b9114d96b4283e5ac3f9b27492221cf2753886eb
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-19 17:35:25 -0500

    NTI-3137 Updating when the content package does overwrites changes you make while you're typing, a new content package should be triggering an update higher up so there is no need to do it here

--- src/editor/meta-editor/Title.jsx
+++ src/editor/meta-editor/Title.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {scoped} from 'nti-lib-locale';
-import {HOC} from 'nti-web-commons';
 
 import {Plugins} from '../../draft-core';
 import {saveContentPackageTitle} from '../Actions';
 
 import MetaEditor from './MetaEditor';
 
-const {ItemChanges} = HOC;
 
 const DEFAULT_TEXT = {
 	placeholder: 'Title'
@@ -32,27 +30,20 @@ export default class ContentEditorTitle extends React.Component {
 	}
 
 
-	onItemChanged = () => {
-		this.forceUpdate();
-	}
-
-
 	render () {
 		const {contentPackage} = this.props;
 		const {title} = contentPackage || {};
 
 		return (
-			<ItemChanges item={contentPackage} onItemChanged={this.onItemChanged}>
-				<MetaEditor
-					className="content-title-editor"
-					fieldName="title"
-					contentPackage={contentPackage}
-					value={title}
-					onContentChange={this.onContentChange}
-					plugins={plugins}
-					placeholder={t('placeholder')}
-				/>
-			</ItemChanges>
+			<MetaEditor
+				className="content-title-editor"
+				fieldName="title"
+				contentPackage={contentPackage}
+				value={title}
+				onContentChange={this.onContentChange}
+				plugins={plugins}
+				placeholder={t('placeholder')}
+			/>
 		);
 	}
 }
