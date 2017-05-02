fd99ed18e8cec45bdb44e94b330599cf981b2d22
Andrew Ligon <andrew.ligon@nextthought.com>
2017-05-02 18:09:48 -0500

    Checkpoint for outputting RST for atomic blocks

--- src/RST/parser/draft-to-rst/blocks/index.js
+++ src/RST/parser/draft-to-rst/blocks/index.js
@@ -1,9 +1,11 @@
+import Atomic from './Atomic';
 import Header from './Header';
 import OrderedListItem from './OrderedListItem';
 import Paragraph from './Paragraph';
 import UnorderedListItem from './UnorderedListItem';
 
 export default [
+	Atomic,
 	Header,
 	OrderedListItem,
 	Paragraph,
