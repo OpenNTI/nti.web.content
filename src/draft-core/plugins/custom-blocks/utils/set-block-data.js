c643a112f4db96cde97e9097bf01a1cbdb52931f
Andrew Ligon <andrew.ligon@nextthought.com>
2017-05-03 11:36:58 -0500

    Create an immutable map, so our data doesn't get converted into something we don't expect

--- src/draft-core/plugins/custom-blocks/utils/set-block-data.js
+++ src/draft-core/plugins/custom-blocks/utils/set-block-data.js
@@ -1,4 +1,6 @@
 import {Modifier, EditorState} from 'draft-js';
+//We don't really need immutable its just something draft needs so let draft depend on it
+import {Map as ImmutableMap} from 'immutable';//eslint-disable-line import/no-extraneous-dependencies
 
 import {getRangeForBlock} from '../../../utils';
 import {CHANGE_TYPES} from '../../../Constants';
@@ -7,7 +9,7 @@ export default function setBlockData (block, data, editorState) {
 	const content = editorState.getCurrentContent();
 	const selection = getRangeForBlock(block);
 
-	const newContent = Modifier.setBlockData(content, selection, data);
+	const newContent = Modifier.setBlockData(content, selection, new ImmutableMap(data));
 
 	return EditorState.push(editorState, newContent, CHANGE_TYPES.CHANGE_BLOCK_DATA);
 }
