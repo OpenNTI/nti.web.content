d8e805abf8498a2fc9a4494c774190fd596cb010
Andrew Ligon <andrew.ligon@nextthought.com>
2017-05-09 14:57:11 -0500

    Wire up changing an image

--- src/editor/block-types/Controls.jsx
+++ src/editor/block-types/Controls.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import {scoped} from 'nti-lib-locale';
+
+const DEFAULT_TEXT = {
+	change: 'Replace Image'
+};
+
+const t = scoped('course-figure-controls', DEFAULT_TEXT);
 
 export default class BlockTypeControls extends React.Component {
 	static propTypes = {
 		className: PropTypes.string,
-		onRemove: PropTypes.func
+		onRemove: PropTypes.func,
+		onChange: PropTypes.func
 	}
 
 
@@ -18,6 +26,15 @@ export default class BlockTypeControls extends React.Component {
 	}
 
 
+	onChange = () => {
+		const {onChange} = this.props;
+
+		if (onChange) {
+			onChange();
+		}
+	}
+
+
 	render () {
 		const {className} = this.props;
 		const cls = cx('custom-block-type-control', className);
@@ -25,6 +42,10 @@ export default class BlockTypeControls extends React.Component {
 		return (
 			<div className={cls}>
 				<div className="spacer" />
+				<div className="change" onClick={this.onChange}>
+					<i className="icon-image" />
+					<span>{t('change')}</span>
+				</div>
 				<div className="remove" onClick={this.onRemove}>
 					<i className="icon-bold-x" />
 				</div>
