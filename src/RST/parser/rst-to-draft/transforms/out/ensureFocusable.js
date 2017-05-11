828584013172e956e015c9e8d36d2b1c8e9ee65e
Andrew Ligon <andrew.ligon@nextthought.com>
2017-05-11 16:00:56 -0500

    NTI-3292 ensure there is a focusable space around figures

--- src/RST/parser/rst-to-draft/transforms/out/ensureFocusable.js
+++ src/RST/parser/rst-to-draft/transforms/out/ensureFocusable.js
@@ -1,23 +1,35 @@
 import {BLOCKS} from '../../../../../draft-core';
 
-const emptyParagraph = {
-	type: BLOCKS.UNSTYLED,
-	text: '',
-	depth: 0,
-	inlineStyleRanges: [],
-	entityRanges: []
+const INSERT_BEFORE_SET = new Set([BLOCKS.HEADER_ONE, BLOCKS.HEADER_TWO, BLOCKS.HEADER_THREE, BLOCKS.HEADER_FOUR, BLOCKS.HEADER_FIVE, BLOCKS.HEADER_SIX, BLOCKS.ATOMIC]);
+
+const getEmptyParagraph = () => {
+	return {
+		type: BLOCKS.UNSTYLED,
+		text: '',
+		depth: 0,
+		inlineStyleRanges: [],
+		entityRanges: []
+	};
 };
 
+//TODO: look into a more efficient way to do this
 export default function (output) {
 	const {blocks, entityMap} = output;
-	let newBlocks = blocks;
+	let newBlocks = [];
 
-	if (blocks[0].type === BLOCKS.ATOMIC) {
-		newBlocks = [emptyParagraph, ...newBlocks];
+	for (let i = 0; i < blocks.length; i++) {
+		let block = blocks[i];
+		let prevBlock = blocks[i - 1];
+
+		if (prevBlock && block.type === BLOCKS.ATOMIC && INSERT_BEFORE_SET.has(prevBlock.type)) {
+			newBlocks.push(getEmptyParagraph());
+		}
+
+		newBlocks.push(block);
 	}
 
-	if (blocks[blocks.length - 1].type === BLOCKS.ATOMIC) {
-		newBlocks = [...newBlocks, emptyParagraph];
+	if (newBlocks[newBlocks.length - 1].type === BLOCKS.ATOMIC) {
+		newBlocks.push(getEmptyParagraph());
 	}
 
 	return {blocks: newBlocks, entityMap};
