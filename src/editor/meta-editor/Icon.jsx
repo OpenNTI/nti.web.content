7bcf0fda6434aaf92d702e3601e247db31ad7005
Landon Sherwood <landonsherwood23@gmail.com>
2017-05-23 13:57:55 -0500

    NTI-3026 support drag and drop for native reading icon

--- src/editor/meta-editor/Icon.jsx
+++ src/editor/meta-editor/Icon.jsx
@@ -26,8 +26,14 @@ export default class ContentEditorIcon extends React.Component {
 		course: React.PropTypes.object
 	}
 
+	constructor (props) {
+		super(props);
+		this.state = {fileOver: false, counter: 0};
+	}
 
-	onClick = () => {
+	onClick = (e) => {
+		e.preventDefault();
+
 		const {course, contentPackage} = this.props;
 
 		const accept = x => !x.isFolder && fileIsImage(x);
@@ -55,17 +61,78 @@ export default class ContentEditorIcon extends React.Component {
 	}
 
 
+	dragOver = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+	}
+
+
+	dragEnter = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		this.setState({ fileOver: true, counter: this.state.counter + 1 });
+	}
+
+
+	dragLeave = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		this.setState({ counter: this.state.counter - 1 });
+		if(this.state.counter - 1 === 0) {
+			this.setState({ fileOver: false });
+		}
+	}
+
+
+	drop = (e) => {
+		const {contentPackage} = this.props;
+		e.preventDefault();
+		e.stopPropagation();
+		this.setState({ fileOver: false });
+
+		let file = e.dataTransfer.items[0].getAsFile();
+		let url = this.createObjectURL(file);
+		saveContentPackageIcon(contentPackage, url);
+	}
+
+
+	getURLObject = () => {
+		let url;
+
+		if (window.URL && window.URL.createObjectURL) {
+			url = window.URL;
+		} else if (window.webkitURL && window.webkitURL.createObjectURL) {
+			url = window.webkitURL;
+		}
+
+		return url;
+	}
+
+
+	createObjectURL = (file) => {
+		let url = this.getURLObject();
+
+		if (!url) { return null; }
+
+		this.objectURL = url.createObjectURL(file);
+
+		return this.objectURL;
+	}
+
+
 	render () {
 		const {contentPackage} = this.props;
 		const {icon} = contentPackage || {};
-		const cls = cx('content-icon-editor', {placeholder: !icon});
+		let {fileOver} = this.state;
+		const cls = cx('content-icon-editor', {placeholder: !icon}, {'file-over': fileOver});
 
 
 		return (
 			<ItemChanges item={contentPackage} onItemChanged={this.onContentPackageChanged}>
-				<div className={cls} onClick={this.onClick}>
+				<div className={cls} onClick={this.onClick} onDragOver={this.dragOver} onDragEnter={this.dragEnter} onDragLeave={this.dragLeave} onDrop={this.drop}>
 					{!icon && this.renderPlaceholder()}
 					{icon && this.renderIcon(icon)}
+					<input type="file" data-qtip="Cover Image" accept="image/*" />
 				</div>
 			</ItemChanges>
 		);
