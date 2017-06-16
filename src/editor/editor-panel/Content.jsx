e4f18d1727d2eb1a11b07c927bb72a582a2785a1
Cory Jones <cory.jones@nextthought.com>
2017-06-16 08:45:14 -0500

    NTI-3102 Add sharing controls to content editor

--- src/editor/editor-panel/Content.jsx
+++ src/editor/editor-panel/Content.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {Associations} from 'nti-web-commons';
 
 import MetaEditor from '../meta-editor';
 import ContentEditor from '../content-editor';
+
+const {Sharing} = Associations;
 
 EditorPanelContent.propTypes = {
 	contentPackage: PropTypes.object,
@@ -11,6 +14,7 @@ EditorPanelContent.propTypes = {
 export default function EditorPanelContent ({contentPackage, course}) {
 	return (
 		<div>
+			<Sharing.Lessons item={contentPackage} scope={course} />
 			<MetaEditor contentPackage={contentPackage} course={course} />
 			<ContentEditor contentPackage={contentPackage} course={course} />
 		</div>
