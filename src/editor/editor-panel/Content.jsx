702c64ffb8749c7988208938b79b7b7fa449d77e
Cory Jones <cory.jones@nextthought.com>
2017-06-22 11:56:04 -0500

    NTI-3102 Move the location of the sharing widget within the meta widget

--- src/editor/editor-panel/Content.jsx
+++ src/editor/editor-panel/Content.jsx
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Associations} from 'nti-web-commons';
 
 import MetaEditor from '../meta-editor';
 import ContentEditor from '../content-editor';
-
-const {Sharing} = Associations;
 
 EditorPanelContent.propTypes = {
 	contentPackage: PropTypes.object,
@@ -14,7 +11,6 @@ EditorPanelContent.propTypes = {
 export default function EditorPanelContent ({contentPackage, course}) {
 	return (
 		<div>
-			<Sharing.Lessons item={contentPackage} scope={course} />
 			<MetaEditor contentPackage={contentPackage} course={course} />
 			<ContentEditor contentPackage={contentPackage} course={course} />
 		</div>
--- src/editor/meta-editor/View.jsx
+++ src/editor/meta-editor/View.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {Associations} from 'nti-web-commons';
 
 import Icon from './Icon';
 import Title from './Title';
 import Description from './Description';
+
+const {Sharing} = Associations;
 
 MetaEditor.propTypes = {
 	contentPackage: PropTypes.object,
@@ -15,6 +18,7 @@ export default function MetaEditor ({contentPackage, course}) {
 			<Icon contentPackage={contentPackage} course={course} />
 
 			<div className="wrap">
+				<Sharing.Lessons item={contentPackage} scope={course} />
 				<Title contentPackage={contentPackage} course={course} />
 				<Description contentPackage={contentPackage} course={course} />
 			</div>
