66a299afad24048e691c79e30becfc1b71a70047
Cory Jones <cory.jones@nextthought.com>
2017-06-26 13:19:05 -0500

    NTI-3483 Move functions out into utils, add tests

--- src/draft-core/plugins/insert-block/utils/index.js
+++ src/draft-core/plugins/insert-block/utils/index.js
@@ -1,2 +1,4 @@
 export getSelectedText from './get-selected-text';
 export insertBlock from './insert-block';
+export moveSelectionToNextBlock from './move-selection-to-next-block';
+export ensureMaintainSelection from './ensure-maintain-selection';
