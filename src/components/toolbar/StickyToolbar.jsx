5dcea6d5c8a5354d1876c559bc41e100cb11bb84
Cory Jones <cory.jones@nextthought.com>
2017-08-23 12:40:53 -0500

    NTI-3863 Pass otherProps from StickyToolbar to Toolbar

--- src/components/toolbar/StickyToolbar.jsx
+++ src/components/toolbar/StickyToolbar.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { StickyElement, StickyContainer } from 'nti-web-commons';
 
 import Toolbar from './Toolbar';
 
 export default class StickyToolbar extends React.Component {
-	static propTypes = {
-		path: PropTypes.any,
-		pageSource: PropTypes.any,
-		toc: PropTypes.object,
-		hideControls: PropTypes.bool,
-		hideHeader: PropTypes.bool,
-		doNavigation: PropTypes.func
-	}
-
 	constructor (props) {
 		super(props);
 	}
 
 	renderToolbar () {
-		return (<Toolbar path={this.props.path} pageSource={this.props.pageSource} doNavigation={this.props.doNavigation}
-			hideControls={this.props.hideControls} hideHeader={this.props.hideHeader} toc={this.props.toc}/>);
+		const {...otherProps} = this.props;
+
+		return (<Toolbar {...otherProps}/>);
 	}
 
 	render () {
