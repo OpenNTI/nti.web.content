51629cadd080f7b45a00c17b1267146de72b0546
Cory Jones <cory.jones@nextthought.com>
2017-08-23 08:47:15 -0500

    NTI-3863 Fix exports

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ export * as DraftCore from './draft-core';
 export Editor from './editor';
 
 export ControlBar from './components/ControlBar';
-export Toolbar from './components/toolbar/Toolbar';
-export StickyToolbar from './components/toolbar/StickyToolbar';
+export * from './components/toolbar';
