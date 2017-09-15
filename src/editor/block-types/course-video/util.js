ad7cd3b0f1f1b2fe236c0e5c37552e034c3d8530
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-15 11:21:52 -0500

    Revert "Change Course-Video to NTIVIDEOREF"
    This reverts commit e5642c12e9d95ded0506f69697e985406eeabdfc.

--- src/editor/block-types/course-video/util.js
+++ src/editor/block-types/course-video/util.js
@@ -1,6 +1,6 @@
 import {BLOCKS} from '../../../draft-core';
 
-const NAME = 'ntivideoref';
+const NAME = 'ntivideo';
 
 export const isVideoBlock = contentBlock => {
 	const type = contentBlock.getType();
