c4bae3112f295b71214c413218b61fafef9b9cab
Cory Jones <cory.jones@nextthought.com>
2017-09-19 15:22:25 -0500

    NTI-4004 Create separate content blocks for videos and videorefs

---
+++ src/editor/block-types/course-video-ref/Picker.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {scoped} from 'nti-lib-locale';
+import {createMediaSourceFromUrl} from 'nti-web-video';
+import {Prompt, DialogButtons, Loading} from 'nti-web-commons';
+import {wait} from 'nti-commons';
+
+import {normalizeSource, parseEmbedCode} from './util';
+
+const DEFAULT_TEXT = {
+	header: 'Enter a link to a YouTube, Vimeo, or Kaltura video.',
+	cancel: 'Cancel',
+	done: 'Done',
+	placeholder: 'Enter a link or embed code',
+	invalid: 'Invalid Link'
+};
+
+const t = scoped('nti-content.editor.block-types.course-video.Picker', DEFAULT_TEXT);
+
+
+async function getMediaSource (rawInput) {
+	const input = rawInput.trim();
+	const url = parseEmbedCode(input) || input;
+	const {service, source} = await createMediaSourceFromUrl(url);
+	const normalizedSource = normalizeSource(service, source);
+
+	return {service, source: normalizedSource};
+}
+
+export default class VideoPicker extends React.Component {
+	static show (value) {
+		return new Promise((fulfill, reject) => {
+			Prompt.modal(
+				<VideoPicker
+					value={value}
+					onSelect={fulfill}
+					onCancel={reject}
+				/>,
+				'content-editor-video-picker-container'
+			);
+		});
+	}
+
+	static propTypes = {
+		value: PropTypes.string,
+		onSelect: PropTypes.func,
+		onCancel: PropTypes.func,
+		onDismiss: PropTypes.func
+	}
+
+	constructor (props) {
+		super(props);
+
+		const {value} = props;
+
+		this.state = {
+			value
+		};
+	}
+
+	componentWillReceiveProps (nextProps) {
+		const {value:nextValue} = nextProps;
+		const {value:prevValue} = this.props;
+
+		if (nextValue !== prevValue) {
+			this.setState({
+				value: nextValue
+			});
+		}
+	}
+
+
+	onCancel = () => {
+		const {onCancel, onDismiss} = this.props;
+
+		onDismiss();
+
+		if (onCancel) {
+			onCancel();
+		}
+	}
+
+
+	onSave = () => {
+		const {value} = this.state;
+		const {onSelect, onDismiss} = this.props;
+
+		this.setState({
+			saving: true
+		}, () => {
+			getMediaSource(value)
+				.then(wait.min(500))
+				.then((source) => {
+					if (onSelect) { onSelect(source); }
+
+					onDismiss();
+				})
+				.catch(() => {
+					this.setState({
+						invalid: true,
+						saving: false
+					});
+				});
+		});
+	}
+
+
+	onInputChange = (e) => {
+		this.setState({
+			value: e.target.value,
+			invalid: false
+		});
+	}
+
+
+	render () {
+		const {value, saving, invalid} = this.state;
+
+		const buttons = [
+			{label: t('cancel'), onClick: () => this.onCancel()},
+			{label: t('done'), onClick: () => this.onSave()}
+		];
+
+		return (
+			<div className="content-editor-video-picker">
+				<div className="picker">
+					<h1 className="heading">{t('header')}</h1>
+					<label>
+						<span>Link</span>
+						<input type="text" placeholder={t('placeholder')} value={value} onChange={this.onInputChange} />
+					</label>
+					{invalid && (<span className="error">{t('invalid')}</span>)}
+				</div>
+				<DialogButtons buttons={buttons} />
+				<div className={cx('saving-mask', {saving})}>
+					{saving && <Loading.Spinner />}
+				</div>
+			</div>
+		);
+	}
+}
--- src/editor/block-types/course-video/index.js
+++ src/editor/block-types/course-video/index.js
@@ -1,10 +1,8 @@
 import {isVideoBlock} from './util';
-import Button from './Button';
 import Editor from './Editor';
 
 
 export const handlesBlock = contentBlock => isVideoBlock(contentBlock);
 export const className = 'nti-course-video-block';
 export const editable = false;
-export const button = Button;
 export const component = Editor;
