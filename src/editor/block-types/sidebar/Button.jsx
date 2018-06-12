09ee2edbf4e216319629a9437a534f8e95eaf610
Andrew Ligon <andrew.ligon@nextthought.com>
2018-06-12 17:20:21 -0500

    Add a sidebar type button

---
+++ src/editor/block-types/sidebar/Button.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
+import {BLOCKS} from '@nti/web-editor';
+
+import Button from '../common/Button';
+
+const t = scoped('web-content.editor.block-types.sidebar.button', {
+	label: 'Sidebar'
+});
+
+function createBlock (insertBlock) {
+	insertBlock({
+		type: BLOCKS.ATOMIC,
+		text: '',
+		data: {
+			name: 'sidebar',
+			arguments: 'Sidebar',
+			body: [],
+			options: {}
+		}
+	});
+}
+
+function isBlock (block) {
+	const type = block.getType();
+	const data = block.getData();
+
+	return type === BLOCKS.ATOMIC && data.get('name') === 'sidebar';
+}
+
+export default function SidebarButton () {
+	return (
+		<Button
+			className="content-editor-sidebar-button"
+			iconClass="content-editor-block-types-sidebar-icon"
+			label={t('label')}
+			createBlock={createBlock}
+			isBlockPredicate={isBlock}
+		/>
+	);
+}
