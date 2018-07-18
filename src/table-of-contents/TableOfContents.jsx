5d1967f9af79fe063bf519ef2297223fc72c3fe7
Andrew Ligon <andrew.ligon@nextthought.com>
2018-07-18 17:58:42 -0500

    NTI-6078 build fake nodes for real pages since they may not have a toc node

--- src/table-of-contents/TableOfContents.jsx
+++ src/table-of-contents/TableOfContents.jsx
@@ -13,6 +13,41 @@ const DEFAULT_TEXT = {
 
 const t = scoped('web-content.table-of-contents.toc', DEFAULT_TEXT);
 
+function buildFakeNodeForPage (realPage) {
+	const {page, NTIID, NavNTIID} = realPage;
+
+	if (!NavNTIID || !NTIID) { return null; }
+
+	const navParts = NavNTIID.split('#');
+	const isFragment = navParts.length > 1;
+
+	return {
+		type: 'part',
+		id: NTIID,
+		parent: !isFragment ? null : {
+			id: navParts[0],
+			getAttribute (attr) {
+				if (attr === 'href') {
+					return navParts[0];
+				}
+			}
+		},
+		isAnchor () {
+			return navParts.length > 1;
+		},
+		getAnchorTarget () {
+			return navParts[1];
+		},
+		title: t('realPageTitle', {page}),
+		getAttribute (attr) {
+			if (attr === 'href') {
+				return NavNTIID;
+			}
+		},
+		children: []
+	};
+}
+
 TableOfContents.propTypes = {
 	toc: PropTypes.object.isRequired,
 	filter: PropTypes.string,
@@ -31,11 +66,15 @@ export default function TableOfContents ({toc, filter, onSelectNode}) {
 			{hasRealPages && (
 				<div className="real-pages">
 					{realPages.map((realPage, index) => {
-						const {page, node} = realPage;
+						const node = buildFakeNodeForPage(realPage);
 						const {id: rootId} = root || {};
 
+						if (!node) {
+							return null;
+						}
+
 						return (
-							<Node key={index} root={rootId} node={node} title={t('realPageTitle', {page})} type="part" onSelectNode={onSelectNode} />
+							<Node key={index} root={rootId} node={node} onSelectNode={onSelectNode} />
 						);
 					})}
 				</div>
