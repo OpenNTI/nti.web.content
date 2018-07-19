3f44a7974db2ff72ccc93a8bf46b8e74bf7ddbce
Josh Birdwell <jbirdwell34@gmail.com>
2018-07-19 18:50:34 -0500

    NTI-6209: component did catch

--- src/common/PanelSidebar.jsx
+++ src/common/PanelSidebar.jsx
@@ -2,27 +2,38 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import cx from 'classnames';
 import {FixedElement} from '@nti/web-commons';
+import Logger from '@nti/util-logger';
 
+const logger = Logger.get('lib:content-editor:PanelSidebar');
+
+export default class PanelSidebar extends React.Component {
+
+	componentDidCatch (error, info) {
+		logger.error(error);
+		this.setState({ error });
+	}
+
+	render () {
+		const {className, sidebar, children} = this.props;
+		const cls = cx('panel-sidebar', className);
+
+		return (
+			<div className={cls}>
+				<div className="panel">
+					{children}
+				</div>
+				<div className="sidebar">
+					<FixedElement>
+						{sidebar}
+					</FixedElement>
+				</div>
+			</div>
+		);
+	}
+}
 
 PanelSidebar.propTypes = {
 	className: PropTypes.string,
 	sidebar: PropTypes.node,
 	children: PropTypes.any
 };
-export default function PanelSidebar ({className, sidebar, children}) {
-	const cls = cx('panel-sidebar', className);
-
-	return (
-		<div className={cls}>
-			<div className="panel">
-				{children}
-			</div>
-			<div className="sidebar">
-				<FixedElement>
-					{sidebar}
-				</FixedElement>
-			</div>
-		</div>
-	);
-
-}
