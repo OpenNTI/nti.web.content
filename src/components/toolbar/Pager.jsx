f9e45e8602da1f2e9ece60d05bfd1592123522e3
Josh Birdwell <jbirdwell34@gmail.com>
2018-08-06 12:46:16 -0500

    NTI-6333: Use old pager if there is a page source

---
+++ src/components/toolbar/Pager.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { scoped } from '@nti/lib-locale';
+
+import CurrentPage from './CurrentPage';
+
+const t = scoped('content.toolbar.Pager', {
+	separator: ' of '
+});
+
+
+// TODO: Use getroute for in the future
+export default class Pager extends React.Component {
+	static propTypes = {
+		doNavigation: PropTypes.func.isRequired,
+		hideControls: PropTypes.bool.isRequired,
+		pageSource: PropTypes.shape({
+			getPrevious: PropTypes.func.isRequired,
+			getPreviousTitle: PropTypes.func.isRequired,
+			getPreviousPrecache: PropTypes.func.isRequired,
+			getNext: PropTypes.func.isRequired,
+			getNextTitle: PropTypes.func.isRequired,
+			getNextPrecache: PropTypes.func.isRequired
+		})
+	};
+
+	renderPage () {
+		const { doNavigation, pageSource } = this.props;
+
+		if (!pageSource) {
+			return null;
+		}
+		return (
+			<div className="page">
+				<CurrentPage
+					pageSource={pageSource}
+					doNavigation={doNavigation}
+				/>
+				<span className="separator">{t('separator')}</span>
+				<span className="total">
+					{pageSource.getTotal()}
+				</span>
+			</div>
+		);
+	}
+
+	goToPrevious = () => {
+		const { doNavigation, pageSource } = this.props;
+
+		if (pageSource && pageSource.previous) {
+			const previous = pageSource.getPrevious();
+			const title = pageSource.getPreviousTitle();
+			const precache = pageSource.getPreviousPrecache();
+			return doNavigation(title, previous, precache);
+		}
+
+		return;
+	};
+
+	renderPrev () {
+		const { pageSource } = this.props;
+		const className =
+			'prev' +
+			(pageSource && pageSource.previous && pageSource.getPrevious()
+				? ''
+				: ' disabled');
+		return <div onClick={this.goToPrevious} className={className} />;
+	}
+
+	goToNext = () => {
+		const { pageSource } = this.props;
+		const { doNavigation } = this.props;
+
+		if (pageSource && pageSource.next) {
+			const next = pageSource.getNext();
+			const title = pageSource.getNextTitle();
+			const precache = pageSource.getNextPrecache();
+			return doNavigation(title, next, precache);
+		}
+
+		return;
+	};
+
+	renderNext () {
+		const { pageSource } = this.props;
+		const className =
+			'next' +
+			(pageSource && pageSource.next && pageSource.getNext()
+				? ''
+				: ' disabled');
+		return <div onClick={this.goToNext} className={className} />;
+	}
+
+	render () {
+		if (this.props.hideControls) {
+			return null;
+		}
+
+		return (
+			<div className="non-content-pager controls">
+				{this.renderPage()}
+				{this.renderPrev()}
+				{this.renderNext()}
+			</div>
+		);
+	}
+}
