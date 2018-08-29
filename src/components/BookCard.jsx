c79a5191cd42f1f5c468397b06b6c1efb7cc1956
Landon Sherwood <landonsherwood23@gmail.com>
2018-08-29 14:44:44 -0500

    Add book card for new library

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {LinkTo} from '@nti/web-routing';
+
+import defaultImage from '../assets/default-card-image.png';
+
+BookCard.propTypes = {
+	className: PropTypes.string,
+	bundle: PropTypes.object.isRequired
+};
+
+export default function BookCard ({className, bundle, ...props}) {
+	let {byline, author, title} = bundle || {};
+
+	return (
+		<LinkTo.Object object={bundle}>
+			<div className={cx('book-card', className)} {...props} >
+				<div className="bundle-card-image">
+					<img src={defaultImage} />
+				</div>
+				<label>
+					<h3 className="book-title">{title}</h3>
+					<address className="book-author">By {byline || author}</address>
+				</label>
+			</div>
+		</LinkTo.Object>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export Editor from './editor';
 export * as TableOfContents from './table-of-contents/';
 export ControlBar from './components/ControlBar';
 export ContentNavMenu from './components/navigation/ContentNavMenu';
+export BookCard from './components/BookCard';
 export * from './components/toolbar';
 export * from './toolbar';
 export Stream from './stream';
