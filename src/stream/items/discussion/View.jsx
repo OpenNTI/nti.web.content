55fb026cbcfa0adea3c0cf562cb78bcf006aa264
Josh Birdwell <jbirdwell34@gmail.com>
2018-11-05 16:15:56 -0600

    progress on discussions

--- src/stream/items/discussion/View.jsx
+++ src/stream/items/discussion/View.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { LuckyCharms, DisplayName, DateTime } from '@nti/web-commons';
 import { LinkTo } from '@nti/web-routing';
 import { scoped } from '@nti/lib-locale';
+import { Panel as Body } from '@nti/web-modeled-content';
 
 const t = scoped('content.stream.items.note.Detail', {
 	duration: '%(duration)s ago'
@@ -14,7 +15,14 @@ import Registry from '../Registry';
 @Registry.register('application/vnd.nextthought.forums.communityheadlinetopic')
 class Discussion extends React.Component {
 	static propTypes = {
-		item: PropTypes.object.isRequired,
+		item: PropTypes.shape({
+			getCreatedTime: PropTypes.func.isRequired,
+			creator: PropTypes.string.isRequired,
+			title: PropTypes.string.isRequired,
+			headline: PropTypes.shape({
+				body: PropTypes.array.isRequired
+			}).isRequired
+		}).isRequired,
 		context: PropTypes.object
 	}
 
@@ -25,22 +33,24 @@ class Discussion extends React.Component {
 
 		return (
 			<div className="stream-discussion">
-				{/* <Breadcrumb className="discussion-breadcrumb" item={item} context={context} />
-				<div className="discussion-body">
+				<Breadcrumb className="discussion-breadcrumb" item={item} context={context} />
+				<div className="item">
 					<LuckyCharms item={item} />
-					<div className="discussion-header">
-						<Avatar creator={creator} />
-						<div className="discussion-meta">
-							<div className="title">{title}</div>
+
+					<Avatar creator={creator} />
+					<div className="discussion-meta">
+						<div className="subject">{title}</div>
+						<div className="stamp">
 							<LinkTo.Object object={{ Username: creator, isUser: true }} context="stream-profile">
 								<DisplayName entity={creator} />
 							</LinkTo.Object>
-							<div className="created-time">
+							<span className="time">
 								{t('duration', { duration: DateTime.getNaturalDuration(Date.now() - created, 1) })}
-							</div>
+							</span>
 						</div>
 					</div>
-				</div> */}
+					<Body className="body" body={item.headline && item.headline.body} />
+				</div>
 			</div>
 		);
 	}
