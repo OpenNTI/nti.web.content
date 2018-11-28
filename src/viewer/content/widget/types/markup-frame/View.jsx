de399d5fc2f74ada97be2f0b35ff625a78ecc64d
Andrew Ligon <andrew.ligon@nextthought.com>
2018-11-28 10:23:12 -0600

    remove unused deps

--- src/viewer/content/widget/types/markup-frame/View.jsx
+++ src/viewer/content/widget/types/markup-frame/View.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {isEmpty} from '@nti/lib-commons';
-import {Card, Prompt, ZoomableContent} from '@nti/web-commons';
+import {Card} from '@nti/web-commons';
 import {scoped} from '@nti/lib-locale';
 import cx from 'classnames';
 
