4001571f187c7caba7d27e77c3179b3bd7c31ef1
Ray Hatfield <ray.hatfield@nextthought.com>
2019-03-25 14:55:07 -0500

    NTI-7502 - move NoteSummary to web-discussions

--- test/app/transcripted-video-test/Sidebar.jsx
+++ test/app/transcripted-video-test/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import {LinkTo} from '@nti/web-routing';
-import {NoteSummary} from '@nti/web-commons';
+import {NoteSummary} from '@nti/web-discussions';
 
 import styles from './Sidebar.css';
 
