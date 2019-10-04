032f2cfd33270d3fcd0f9de82d6b72427efae02b
Ray Hatfield <ray.hatfield@gmail.com>
2019-10-04 17:47:07 -0500

    NTI-8563 - paint

--- src/table-of-contents/SearchResults.jsx
+++ src/table-of-contents/SearchResults.jsx
@@ -2,42 +2,74 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import {rawContent} from '@nti/lib-commons';
-import {LinkTo} from '@nti/web-routing';
 import {Layouts} from '@nti/web-commons';
 
 import styles from './SearchResults.css';
 
 const cx = classnames.bind(styles);
 
+const bin = (arr, field) => arr.reduce((acc, item) => {
+	acc[item[field]] = [...acc[item[field]] || [], item];
+	return acc;
+}, {});
+
 export default class SearchResults extends React.Component {
-	static propTypes = {
-		hits: PropTypes.array,
-		onLoadMore: PropTypes.func
-	}
 
 	render () {
 		const {hits, onLoadMore} = this.props;
 		return !hits || !hits.length ? null : (
 			<div className="toc-search-results">
 				<Layouts.InfiniteScroll.Continuous loadMore={onLoadMore} buffer={100}>
-					{hits.map((hit, i) => {
-						const {NTIID: id, ContainerTitle: title, Fragments: fragments, TargetMimeType: targetMimeType} = hit;
-
-						return (
-							<LinkTo.Object object={hit} key={id || i} className={cx('search-hit')} data-target-mimetype={targetMimeType}>
-								<div className={cx('hit-title')}>{title}</div>
-								<div className={cx('hit-fragments')}>
-									{fragments.map((f, idx) => <Fragment key={idx} fragment={f} />)}
-								</div>
-							</LinkTo.Object>
-						);
-					})}
+					{hits.map((hit, i) => (
+						<Hit key={hit.NTIID} hit={hit} />
+					))}
 				</Layouts.InfiniteScroll.Continuous>
 			</div>
 		);
 	}
 }
 
-const Fragment = ({fragment: {Matches: matches}}) => (
-	matches.map((m, i) => <span key={i} {...rawContent(m)} />)
+SearchResults.propTypes = {
+	hits: PropTypes.array,
+	onLoadMore: PropTypes.func
+};
+
+const Hit = ({hit: { ContainerTitle: containerTitle, Fragments: fragments, TargetMimeType: targetMimeType }}) => {
+	const {title, content = [], keywords} = bin(fragments, 'Field');
+	return (
+		<div className={cx('search-hit')} data-target-mimetype={targetMimeType}>
+			<div className={cx('hit-title')}>
+				{title
+					? <Fragment fragment={title[0]} />
+					: containerTitle
+				}
+			</div>
+			<Fragments fragments={content} className={cx('content-hits')}/>
+			<Fragments fragments={keywords} className={cx('keyword-hits')} tag="div" />
+		</div>
+	);
+};
+
+Hit.propTypes = {
+	hit: PropTypes.shape({
+		ContainerTitle: PropTypes.string,
+		Fragments: PropTypes.array,
+		TargetMimeType: PropTypes.string,
+	})
+};
+
+const Fragments = ({fragments, className, ...other}) => (
+	!fragments || !fragments.length ? null : (
+		<div className={cx('hit-fragments', className)}>
+			{ fragments.map((f, i) => <Fragment key={i} fragment={f} {...other} />)}
+		</div>
+	)
+);
+
+Fragments.propTypes = {
+	fragments: PropTypes.array
+};
+
+const Fragment = ({fragment: {Matches: matches}, tag: Tag = 'p'}) => (
+	matches.map((m, i) => <Tag key={i} className={cx('fragment')} {...rawContent(m)} />)
 );
