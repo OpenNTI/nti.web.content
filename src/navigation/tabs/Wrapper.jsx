94af41a2fb5e9996757d3ae97ad59be98813769c
Jonathan Grimes <jsg2021@gmail.com>
2020-10-21 23:38:13 -0500

    simplify, useLocation hook

--- src/navigation/tabs/Wrapper.jsx
+++ src/navigation/tabs/Wrapper.jsx
@@ -1,13 +1,14 @@
 import URL from 'url';
 
+
 import React from 'react';
+import {useLocation} from '@nti/web-routing';
 import PropTypes from 'prop-types';
 
 import View from './View';
 
 function getRouteFromLocation (location) {
 	const {hash, pathname, search} = location;
-
 	return URL.format({
 		pathname,
 		hash,
@@ -15,55 +16,29 @@ function getRouteFromLocation (location) {
 	});
 }
 
-export default class ContentTabsWrapper extends React.Component {
-	static propTypes = {
-		activeRoute: PropTypes.string,
-		baseRoute: PropTypes.string
-	}
+ContentTabsWrapper.propTypes = {
+	activeRoute: PropTypes.string,
+	baseRoute: PropTypes.string
+};
 
-	static contextTypes = {
-		router: PropTypes.object
-	}
-
-
-	get router () {
-		return this.context.router;
-	}
+ContentTabsWrapper.contextTypes = {
+	router: PropTypes.shape({
+		baseroute: PropTypes.string,
+		getRouteFor: PropTypes.func,
+	}).isRequired
+};
 
 
-	get baseRoute () {
-		if (!this.router) {
-			return '';
-		}
+export default function ContentTabsWrapper ({activeRoute, baseRoute, ...otherProps}, {router}) {
+	const location = useLocation();
 
-		return this.router.baseroute;
-	}
+	return (
+		<View
+			{...otherProps}
+			activeRoute={activeRoute || getRouteFromLocation(location)}
+			baseRoute={baseRoute || router.baseroute}
+			getRouteFor={router.getRouteFor}
+		/>
+	);
 
-
-	get activeRoute () {
-		if (!this.router || !this.router.route || !this.router.route.location) {
-			return '';
-		}
-
-		return getRouteFromLocation(this.router.route.location);
-	}
-
-
-	get getRouteFor () {
-		return this.router && this.router.getRouteFor;
-	}
-
-
-	render () {
-		const {activeRoute, baseRoute, ...otherProps} = this.props;
-
-		return (
-			<View
-				{...otherProps}
-				activeRoute={activeRoute || this.activeRoute}
-				baseRoute={baseRoute || this.baseRoute}
-				getRouteFor={this.getRouteFor}
-			/>
-		);
-	}
 }
