8d3463ec34ff72c6d068a20866f33a892fb9d8b7
Andrew Ligon <andrew.ligon@nextthought.com>
2020-11-10 21:56:59 -0600

    NTI-9883 use the editor group to get the focused editor

--- src/editor/controls/View.jsx
+++ src/editor/controls/View.jsx
@@ -1,83 +1,36 @@
 import './View.scss';
 import PropTypes from 'prop-types';
 import React from 'react';
-import {ContextProvider} from '@nti/web-editor';
+import {ContextProvider, EditorGroup} from '@nti/web-editor';
 
 import TypeFormat from './TypeFormat';
 import StyleInsertFormat from './StyleInsertFormat';
 import Status from './Status';
 import Publish from './Publish';
 
-function getEditorForSelection (selection) {
-	const first = selection && selection[0];
-	let value = first && first.value;
-	let editor;
+ContentEditorControls.propTypes = {
+	contentPackage: PropTypes.object,
+	course: PropTypes.object,
+	navigateToPublished: PropTypes.func,
+	readOnly: PropTypes.bool,
+	selectionManager: PropTypes.shape({
+		addListener: PropTypes.func,
+		removeListener: PropTypes.func
+	})
+};
+export default function ContentEditorControls ({contentPackage, course, navigateToPublished, readOnly}) {
+	const editor = EditorGroup.useFocusedEditor();
 
-	//If there is more than one don't return any editor for now
-	if (value && selection.length === 1) {
-		editor = value;
-	}
-
-	return editor;
+	return (
+		<ContextProvider editor={editor}>
+			<div className="content-editor-controls">
+				<TypeFormat  editor={editor} />
+				<StyleInsertFormat editor={editor} />
+				<div className="spacer" />
+				<Status />
+				<Publish contentPackage={contentPackage} navigateToPublished={navigateToPublished} disabled={readOnly}/>
+			</div>
+		</ContextProvider>
+	);
 }
 
-export default class ContentEditorControls extends React.Component {
-	static propTypes = {
-		contentPackage: PropTypes.object,
-		course: PropTypes.object,
-		navigateToPublished: PropTypes.func,
-		readOnly: PropTypes.bool,
-		selectionManager: PropTypes.shape({
-			addListener: PropTypes.func,
-			removeListener: PropTypes.func
-		})
-	}
-
-	constructor (props) {
-		super(props);
-
-		this.state = {};
-	}
-
-
-	componentDidMount () {
-		const {selectionManager} = this.props;
-
-		if (selectionManager) {
-			selectionManager.addListener('selection-changed', this.onSelectionChanged);
-		}
-	}
-
-
-	componentWillUnmount () {
-		const {selectionManager} = this.props;
-
-		if (selectionManager) {
-			selectionManager.removeListener('selection-changed', this.onSelectionChanged);
-		}
-	}
-
-
-	onSelectionChanged = (selection) => {
-		this.setState({selection});
-	}
-
-
-	render () {
-		const {readOnly, contentPackage, navigateToPublished} = this.props;
-		const {selection} = this.state;
-		const editor = getEditorForSelection(selection);
-
-		return (
-			<ContextProvider editor={getEditorForSelection(selection)}>
-				<div className="content-editor-controls">
-					<TypeFormat  editor={editor} />
-					<StyleInsertFormat editor={editor} />
-					<div className="spacer" />
-					<Status />
-					<Publish contentPackage={contentPackage} navigateToPublished={navigateToPublished} disabled={readOnly}/>
-				</div>
-			</ContextProvider>
-		);
-	}
-}
