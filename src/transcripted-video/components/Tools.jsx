ac6642038f6289ebc8c222017402adbd38376d86
Andrew Ligon <aligon90@gmail.com>
2021-05-25 14:54:04 -0500

    add a video toolbar with resume and watched segments

--- src/transcripted-video/components/Tools.jsx
+++ src/transcripted-video/components/Tools.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {scoped} from '@nti/lib-locale';
-import { Button, Icons, Text } from '@nti/web-commons';
+import { scoped } from '@nti/lib-locale';
+import { Button, Icons, Text, Hooks } from '@nti/web-commons';
 import { LinkTo } from '@nti/web-routing';
-import { Resume } from '@nti/web-video/controls';
+import { Resume, WatchedSegments } from '@nti/web-video/controls';
 
 const t = scoped('nti-content.transcripted-video.components.Tools', {
 	mediaViewer: 'Media Viewer',
-	watchHistory: 'Watch History'
+	watchHistory: 'Watch History',
 });
 
 const ToolBar = styled('div')`
@@ -16,43 +16,65 @@ const ToolBar = styled('div')`
 	flex-direction: row;
 	align-items: center;
 	justify-content: flex-start;
+	margin-bottom: 1rem;
 
 	& > *:not(:first-of-type) {
 		margin-left: 1rem;
 	}
 `;
 
-const LinkButton = styled(Button).attrs({inverted: true, rounded: true})``;
+const WatchedContainer = styled('div')`
+	margin-top: 1rem;
+	padding: 1.125rem 1.25rem 0.875rem;
+	background: #f5f6f8;
+`;
+
+const LinkButton = styled(Button).attrs({ inverted: true, rounded: true })``;
 
 const Labels = Text.Translator(t);
 
-export default function VideoTools ({video}) {
+export default function VideoTools({ video }) {
 	const [showWatched, setShowWatched] = React.useState(false);
-	const toggleShowWatched = React.useCallback(() => setShowWatched(!showWatched), [showWatched, setShowWatched]);
+	const toggleShowWatched = React.useCallback(
+		() => setShowWatched(!showWatched),
+		[showWatched, setShowWatched]
+	);
+
+	const watchedId = Hooks.useId('watchedsegments');
 
 	return (
 		<>
 			<ToolBar>
 				<Resume inverted rounded />
-				<LinkTo.Object as={LinkButton} object={video} context={{mediaViewer: true}}>
+				<LinkTo.Object
+					as={LinkButton}
+					object={video}
+					context={{ mediaViewer: true }}
+				>
 					<Icons.MediaViewer />
 					<Labels.Base localeKey="mediaViewer" />
 					{/* <Text.Base>{t('mediaViewer')}</Text.Base> */}
 				</LinkTo.Object>
-				<Button rounded inverted onClick={toggleShowWatched}>
+				<Button
+					rounded
+					inverted
+					onClick={toggleShowWatched}
+					aria-expanded={showWatched}
+					aria-controls={watchedId}
+				>
 					<Text.Base>{t('watchHistory')}</Text.Base>
 					<Icons.Chevron.Down />
 				</Button>
 			</ToolBar>
 			{showWatched && (
-				<div>
-					Watch History
-				</div>
+				<WatchedContainer id={watchedId}>
+					<WatchedSegments />
+				</WatchedContainer>
 			)}
 		</>
 	);
 }
 
 VideoTools.propTypes = {
-	video: PropTypes.object
+	video: PropTypes.object,
 };
